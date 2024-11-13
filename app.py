081c720ba89f78b9e094198d12d77432f1a6fe5f
Nathanael Kuhns <n8kuhns@gmail.com>
2024-11-12 22:52:21 -0500

    reformatted the code and made some minor adjustments

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-"""
-INSTALLING REQUIRED PACKAGES
-Run the following commands to install all required packages.
-python -m pip install --upgrade pip
-python -m pip install --upgrade flask-login
-"""
-
-###############################################################################
-# Imports
-###############################################################################
 from __future__ import annotations
 import os
 from flask import Flask, render_template, url_for, redirect
@@ -16,36 +6,45 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_required
 from flask_login import login_user, logout_user, current_user
 
-# Import from local package files
+# local imports
 from hashing_examples import UpdatedHasher
 from loginforms import RegisterForm, LoginForm
 
-###############################################################################
-# Basic Configuration
-###############################################################################
-
+# =================================================================================
 # Identify necessary files
+# =================================================================================
+
 scriptdir = os.path.dirname(os.path.abspath(__file__))
-dbfile = os.path.join(scriptdir, "users.sqlite3")
-pepfile = os.path.join(scriptdir, "pepper.bin")
+dbfile = os.path.join(scriptdir, "todo.sqlite3")
+pepperfile = os.path.join(scriptdir, "pepper.bin")
+
+# =================================================================================
+# Set up hasher
+# =================================================================================
 
 # open and read the contents of the pepper file into your pepper key
 # NOTE: you should really generate your own and not use the one from the starter
-with open(pepfile, "rb") as fin:
+with open(pepperfile, "rb") as fin:
     pepper_key = fin.read()
+
+# generate our own pepper and write it to the file if the file was empty
+if not pepper_key:
+    pepper_key = UpdatedHasher.random_pepper()
+    with open(pepperfile, "wb") as fout:
+        fout.write(pepper_key)
 
 # create a new instance of UpdatedHasher using that pepper key
 pwd_hasher = UpdatedHasher(pepper_key)
 
+# =================================================================================
 # Configure the Flask Application
+# =================================================================================
+
 app = Flask(__name__)
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
-app.config["SECRET_KEY"] = "correcthorsebatterystaple"
+app.config["SECRET_KEY"] = "droporangemineorate"
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{dbfile}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-# Getting the database object handle from the app
-db = SQLAlchemy(app)
 
 # Prepare and connect the LoginManager to this app
 login_manager = LoginManager()
@@ -54,19 +53,19 @@ login_manager.init_app(app)
 login_manager.login_view = "get_login"  # type: ignore
 login_manager.session_protection = "strong"
 
-
-# function that takes a user id and
+# function that takes a user id and returns that user from the database
 @login_manager.user_loader
 def load_user(uid: int) -> User | None:
     return User.query.get(int(uid))
 
-
-###############################################################################
+# =================================================================================
 # Database Setup
-###############################################################################
+# =================================================================================
 
+# Getting the database object handle from the app
+db = SQLAlchemy(app)
 
-# Create a database model for Users
+# Create database models
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.Unicode, nullable=False)
@@ -87,19 +86,19 @@ class User(UserMixin, db.Model):
 
 
 # remember that all database operations must occur within an app context
+# we'll want to get rid of drop_all() + create_all() in the future
 with app.app_context():
+    db.drop_all()
     db.create_all()  # this is only needed if the database doesn't already exist
 
-###############################################################################
+# =================================================================================
 # Route Handlers
-###############################################################################
-
+# =================================================================================
 
 @app.get("/register/")
 def get_register():
     form = RegisterForm()
     return render_template("register.html", form=form)
-
 
 @app.post("/register/")
 def post_register():
@@ -115,22 +114,22 @@ def post_register():
             db.session.add(user)
             db.session.commit()
             return redirect(url_for("get_login"))
-        else:  # if the user already exists
+        else:
+            # if the user already exists
             # flash a warning message and redirect to get registration form
             flash("There is already an account with that email address")
             return redirect(url_for("get_register"))
-    else:  # if the form was invalid
+    else:
+        # if the form was invalid
         # flash error messages and redirect to get registration form again
         for field, error in form.errors.items():
             flash(f"{field}: {error}")
         return redirect(url_for("get_register"))
 
-
 @app.get("/login/")
 def get_login():
     form = LoginForm()
     return render_template("login.html", form=form)
-
 
 @app.post("/login/")
 def post_login():
@@ -147,21 +146,21 @@ def post_login():
             if next is None or not next.startswith("/"):
                 next = url_for("index")
             return redirect(next)
-        else:  # if the user does not exist or the password is incorrect
+        else:
+            # if the user does not exist or the password is incorrect
             # flash an error message and redirect to login form
             flash("Invalid email address or password")
             return redirect(url_for("get_login"))
-    else:  # if the form was invalid
+    else:
+        # if the form was invalid
         # flash error messages and redirect to get login form again
         for field, error in form.errors.items():
             flash(f"{field}: {error}")
         return redirect(url_for("get_login"))
 
-
 @app.get("/")
 def index():
     return render_template("index.html", current_user=current_user)
-
 
 @app.get("/logout/")
 @login_required
--- hashing_examples.py
+++ hashing_examples.py
@@ -1,17 +1,9 @@
-"""
-INSTALLING REQUIRED PACKAGES
-Run the following command to install all required packages.
-python -m pip install --upgrade pip bcrypt argon2-cffi passlib cryptography
-"""
-
 from cryptography.fernet import Fernet
 from passlib.hash import argon2
-from passlib.hash import bcrypt_sha256
 
-# run a few simple tests in a main function
 def main():
-    pepper_key: bytes = DropboxHasher.random_pepper()
-    hasher: DropboxHasher = DropboxHasher(pepper_key)
+    pepper_key: bytes = UpdatedHasher.random_pepper()
+    hasher: UpdatedHasher = UpdatedHasher(pepper_key)
     hash1: bytes = hasher.hash("password")
     hash2: bytes = hasher.hash("password")
     if hasher.check("password", hash1):
@@ -22,33 +14,6 @@ def main():
         print("BOO")
     else:
         print("YAY")
-
-class DropboxHasher:
-    """Hashes passwords using techniques outlined by Dropbox in 2016"""
-    def __init__(self, pepper_key: bytes):
-        self.pepper: Fernet = Fernet(pepper_key)
-
-    def hash(self, pwd: str) -> bytes:
-        # hash with sha256 then run 12 rounds of bcrypt
-        # a random salt is generated per-password automatically
-        hash: str = bcrypt_sha256.using(rounds=12).hash(pwd)
-        # convert this unicode hash string into bytes before encryption
-        hashb: bytes = hash.encode('utf-8')
-        # encrypt this hash using the global pepper
-        pep_hash: bytes = self.pepper.encrypt(hashb)
-        return pep_hash
-
-    def check(self, pwd: str, pep_hash: bytes) -> bool:
-        # decrypt the hash using the global pepper
-        hashb: bytes = self.pepper.decrypt(pep_hash)
-        # convert this hash back into a unicode string
-        hash: str = hashb.decode('utf-8')
-        # check if the given password matches this hash
-        return bcrypt_sha256.verify(pwd, hash)
-
-    @staticmethod
-    def random_pepper() -> bytes:
-        return Fernet.generate_key()
 
 class UpdatedHasher:
     """Upgrades the Dropbox for modern systems using Argon2"""
