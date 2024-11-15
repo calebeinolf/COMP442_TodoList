4aaf7d9af64176574e2d8680123e59fe54e77ca5
DavidJPLeRoux <djpleroux@gmail.com>
2024-11-14 22:32:34 -0500

    Revert "Added AI Button"
    This reverts commit 6f5497e2a15ddbcae70f299c7573695420c59840.

--- static/scripts/index.ts
+++ static/scripts/index.ts
@@ -1,23 +1,3 @@
 document.addEventListener("DOMContentLoaded", async () => {
   console.log("hello");
-  const askAIButton = <HTMLButtonElement> document.getElementById("ask_ai_button");
-  askAIButton.addEventListener("click", aiButtonClicked);
-  const closeModalBtn = <HTMLButtonElement> document.getElementById("closeAIModal")
-  const closeModalBtn2 = <HTMLButtonElement> document.getElementById("submitAIModal")
-  closeModalBtn.addEventListener("click", closeAIModal)
-  closeModalBtn2.addEventListener("click", closeAIModal)
 });
-
-async function aiButtonClicked(){
-  const modal = <HTMLElement> document.getElementById('aiModal');
-  modal.style.display = 'block';
-}
-
-async function closeAIModal(){
-  const modal = <HTMLElement> document.getElementById('aiModal');
-  modal.style.display = 'none';
-}
-
-async function submitAIModal(){
-
-}
